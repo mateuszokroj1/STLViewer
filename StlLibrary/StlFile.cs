cf1e94cc89d12be8737f856a0bc03cb5aef455b1
Mateusz Okrój <mateuszokroj1@gmail.com>
2018-11-13 01:28:26 +0100

    Added Library and WPF project

---
+++ StlLibrary/StlFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace StlLibrary
+{
+    /// <summary>
+    /// Functionality for STL file format
+    /// </summary>
+    public abstract class StlFile
+    {
+
+    }
+}
